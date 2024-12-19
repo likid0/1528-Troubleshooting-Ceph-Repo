98ce80808cdfc8c248863245afebbe924ac4b874
Daniel Parkes <dparkes@redhat.com>
2024-12-19 22:39:30 +0100

    remove build

--- user_exit.sh
+++ user_exit.sh
@@ -2,14 +2,16 @@
 # Get helper file for workstation
 USER_HOME=$(cat /etc/passwd | grep ^U | cut -d: -f6)
 USER=$(cat /etc/passwd | grep ^U | cut -d: -f1)
-cp ./cli-helper-1527.adoc $USER_HOME/cli-helper-1527.adoc
-cp ./cli-helper-1527.html $USER_HOME/cli-helper-1527.html
 mkdir /root/scripts
+mkdir /root/docs
+mkdir $USER_HOME/docs
 cp ./deploy_cluster.sh /root/scripts
 cp ./break_and_fix1.yaml /root/scripts
 cp ./break_and_fix2.yaml /root/scripts
-chown $USER $USER_HOME/cli-helper-1527*
-chmod 644 $USER_HOME/cli-helper-1527*
+cp -pr ./build /root/docs
+cp -pr ./build $USER_HOME/docs
+chown -R $USER $USER_HOME/docs
+chmod -R 644 $USER_HOME/docs
 ssh ceph-node1 "mkdir /root/scripts"
 scp ./purge_cluster.sh root@ceph-node1:/root/scripts
 scp ./new_cluster_deploy.sh root@ceph-node1:/root/scripts
@@ -52,7 +54,8 @@ EOF
 fi
 
 ## Define the path to the local HTML file
-HTML_FILE_PATH="$USER_HOME/cli-helper-1527.html"
+#HTML_FILE_PATH="$USER_HOME/cli-helper-1527.html"
+HTML_FILE_PATH="$USER_HOME/docs/build/site/index.html"
 
 ## Convert the file path to a URL format
 FILE_URL="file://$HTML_FILE_PATH"
